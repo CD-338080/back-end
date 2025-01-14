9c05708dee25fc75c0cb9143f0f3be024d394dba
CD-338080 <elcorreoqueregistraste@gmail.com>
2025-01-14 14:34:16 -0600

    Subida inicial del proyecto

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx,html}", // Asegúrate de que todos tus archivos fuente están incluidos
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
